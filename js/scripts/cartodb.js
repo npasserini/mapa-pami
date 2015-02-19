25f81a49c0fcfa09ea760223fd5b863a9441fcaa
Nicolás Passerini <npasserini@gmail.com>
2015-02-19 02:21:00 -0300

    Reimplementación usando streams

--- js/scripts/cartodb.js
+++ js/scripts/cartodb.js
@@ -1,5 +1,7 @@
 var _ = require('underscore');
 var request = require('superagent');
+var CartoDB = require('cartodb');
+var es = require('event-stream');
 require('string-format').extend(String.prototype);
 
 // var fetchLocations = function(filter, subtipos, bounds, callback, config) {
@@ -7,6 +9,7 @@ require('string-format').extend(String.prototype);
 var config = {
   user: 'nicopasserini',
   table: 'actividadesconcentros',
+  api_key: 'cc36c820c088af10889651e90170f03522c4be32',
   debug: true
 };
 
@@ -25,83 +28,89 @@ function nombreProv(id) {
   return provincias[id - 1];
 }
 
-// http://nicopasserini.cartodb.com/api/v2/sql?q=select count(*) from actividadesconcentros where the_geom is null
+// function cartodb(query, callback) {
+//   debug(query);
 
-var query = "select {fields} from {table} where the_geom is null".format({
-// var query = "select {fields} from {table} where cartodb_id=6624".format({
+//   var url = 'http://{config.user}.cartodb.com/api/v2/sql?q={query}&api_key={config.apikey}'.format({
+//     query: encodeURIComponent(query),
+//     config: config,
+//   });
+//   debug(url);
+
+//   request.get(url, function(err, res) { if (err) throw err; else callback(res); });
+// }
+
+function queryCartoDB(query, stream) {
+  var client = new CartoDB(config);
+
+  client.on('connect', function() {
+    client.query(query, {});
+  });
+
+  client.pipe(es.parse()).on('data', function(data) {
+    debug("Data received..."); 
+    es.readArray(data.rows).pipe(stream);
+  });
+
+  client.connect();
+  debug("Connecting...");
+}
+
+function lookupGeom(row, callback) {
+  row.provincia = nombreProv(row.provincia);
+  var nominatimQuery = '{calle} {numero}, {localidad}, {partido}, {provincia}, Argentina'.format(row);
+
+  var nominatimUrl = 'http://nominatim.openstreetmap.org/search/{query}?format=json'.format({
+    query: encodeURIComponent(nominatimQuery)
+  });
+
+  debug(nominatimUrl);        
+  request.get(nominatimUrl, function(err, res) {
+    debug(nominatimQuery);
+
+    if (err) {
+      debug("Error de comunicación al procesar la fila {}, se debe reintentar", row.cartodb_id);
+      callback(); 
+    }
+    else if(res.status != 200) {
+      callback({cartodb_id: row.cartodb_id, status: res.status} );
+    }
+    else if(res.body.length == 0) {
+      callback({cartodb_id: row.cartodb_id, status: -1} ); // No results
+    }
+    else {
+      var newData = {
+        cartodb_id: row.cartodb_id,
+        geom: geom(res.body[0])
+      }
+
+      debug(newData);
+      callback(null, newData);
+    }
+
+    //   var updateQuery = "update {table} set the_geom={geom}, status=200 where cartodb_id={row.cartodb_id}".format({
+    //     row: row,
+    //     geom: geom(res.body[0]),
+    //     table: config.table
+    //   });
+
+    //   cartodb(updateQuery, function(res) {
+    //     debug("Status: ", res.status);
+    //     if(res.status != 200) debug(res.error);
+    //   });
+    // }
+
+    // debug();
+  });
+}
+
+var query = "select {fields} from {table} where status = 0".format({
   fields: 'cartodb_id, ST_AsGeoJSON(the_geom), calle, numero, localidad, partido, provincia',
   table: config.table
 });
-debug(query);
 
-var url = 'http://{user}.cartodb.com/api/v2/sql?q={query}'.format({
-  user: config.user,
-  query: encodeURIComponent(query)
-});
-debug(url);
-debug();
+var lookupGeomStream = es.map(lookupGeom);
+lookupGeomStream.on('error', function(err) { debug("No se obtuvo la dirección de ", err); });
+// lookupGeomStream.on('data', debug);
 
-request.get(url, function(err, res) {
-  if (err) throw err;
-  if (!res.body.rows) {
-    console.log(res.error.text);
-  }
-  else {
-    _(res.body.rows).each(function(row) {
-      row.provincia = nombreProv(row.provincia);
-      if(!row.st_asgeojson) {
-        var nominatimQuery = '{calle} {numero}, {localidad}, {partido}, {provincia}, Argentina'.format(row);
-
-        var nominatimUrl = 'http://nominatim.openstreetmap.org/search/{query}?format=json'.format({
-          query: encodeURIComponent(nominatimQuery)
-        });
-
-        request.get(nominatimUrl, function(err, res) {
-          debug(nominatimQuery);
-          debug(nominatimUrl);        
-
-          if (err) throw err;
-          else if(res.status != 200) {
-            debug("Error: ", res.status);
-          }
-          else if(res.body.length == 0) {
-            debug("No results");
-          }
-          else {
-            var updateQuery = "update {table} set the_geom={geom} where cartodb_id={row.cartodb_id}".format({
-              row: row,
-              geom: geom(res.body[0]),
-              table: config.table
-            });
-            debug(updateQuery);
-
-            var updateUrl = 'http://{config.user}.cartodb.com/api/v2/sql?q={query}&api_key={apikey}'.format({
-              query: encodeURIComponent(updateQuery),
-              config: config,
-              apikey: 'cc36c820c088af10889651e90170f03522c4be32' 
-            });
-            debug(updateUrl)
-
-            request.get(updateUrl, function(err, res) {
-              if (err) throw err;
-              else {
-                debug("Status: ", res.status);
-                if(res.status != 200) debug(res.error);
-              }
-            });
-          }
-
-          debug();
-        });
-      }
-    });
-
-    // var position = JSON.parse(res.body.rows[0].st_asgeojson);
-    // if (position) console.log("Position from CartoDB: ", position.coordinates);
-  }
-});
-
-
-
-
-
+queryCartoDB(query, lookupGeomStream);
